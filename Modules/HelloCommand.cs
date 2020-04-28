45642fe1971d520a969d6cb1fb430997428a03ff
Deso <johnseth97@gmail.com>
2020-04-27 22:14:30 -0400

    Removed Commands.cs, and split each command into its own module. This is how commands will be handled from now on.

---
+++ Modules/HelloCommand.cs
@@ -0,0 +1,30 @@
+using Discord.Commands;
+using Discord;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+
+namespace GeneralPurposeBot.Modules
+{
+    public class HelloCommand : ModuleBase
+    {
+        [Command("hello")]
+        public async Task Hello()
+        {
+            // initialize empty string builder for reply
+            var sb = new StringBuilder();
+
+            // get user info from the Context
+            var user = Context.User;
+
+            // build out the reply
+            sb.AppendLine($"You are -> [{user.Username}]");
+            sb.AppendLine("I must now say, World!");
+
+            // send simple string reply
+            await ReplyAsync(sb.ToString());
+        }
+    }
+}
