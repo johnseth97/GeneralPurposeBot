62876498cfa74528bc13e1a96533ed034d88bdeb
Deso <johnseth97@gmail.com>
2020-04-28 00:20:23 -0400

    Fixed how the bot gets the user's nickname, and added a reply for when a user invokes a NSFW command in a SFW channel.

--- Modules/HelloCommand.cs
+++ Modules/HelloCommand.cs
@@ -20,7 +20,7 @@ namespace GeneralPurposeBot.Modules
             var user = Context.User;
 
             // build out the reply
-            sb.AppendLine($"You are -> [{user.Nickname}]");
+            sb.AppendLine($"You are -> [{context.Message.Author}]");
             sb.AppendLine("I must now say, World!");
 
             // send simple string reply
--- Services/CommandHandler.cs
+++ Services/CommandHandler.cs
@@ -76,7 +76,7 @@ namespace GeneralPurposeBot.Services
             // if a command isn't found, log that info to console and exit this method
             if (!command.IsSpecified)
             {
-                System.Console.WriteLine($"Command failed to execute for [{context.User.Nickname}] <-> [{result.ErrorReason}]!");
+                System.Console.WriteLine($"Command failed to execute for [{context.Message.Author}] <-> [{result.ErrorReason}]!");
                 return;
             }
 
@@ -84,13 +84,16 @@ namespace GeneralPurposeBot.Services
             // log success to the console and exit this method
             if (result.IsSuccess)
             {
-                System.Console.WriteLine($"Command [{command.Value.Name}] executed for -> [{context.User.Nickname}]");
+                System.Console.WriteLine($"Command [{command.Value.Name}] executed for -> [{context.Message.Author}]");
                 return;
             }
-
+            else if (result == "UnmetPrecondition: This command may only be invoked in an NSFW channel.")
+            {
+                await context.Channel.SendMessageAsync($"Sorry, {context.Message.Author}, you gotta be in a NSFW channel to use this condition");
+            }
 
             // failure scenario, let's let the user know
-            await context.Channel.SendMessageAsync($"Sorry, {context.User.Nickname}... something went wrong -> [{result}]!");
+            await context.Channel.SendMessageAsync($"Sorry, {context.Message.Author}... something went wrong -> [{result}]!");
         }
     }
 }
