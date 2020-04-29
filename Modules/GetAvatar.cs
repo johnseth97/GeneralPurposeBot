6f262d4b00a6778420cf5206b7fff424a0890000
Mitchell Monahan <me@mitchellmonahan.com>
2020-04-29 15:03:36 -0400

    Remove dummy HttpClient class, make avatar command directly upload the image.

--- Modules/GetAvatar.cs
+++ Modules/GetAvatar.cs
@@ -6,22 +6,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System;
+using GeneralPurposeBot.Services;
+using System.Net.Http;
 
 namespace GeneralPurposeBot.Modules
 {
     public class GetAvatar : ModuleBase
     {
+        private HttpClient _client;
+        public GetAvatar(HttpClient client)
+        {
+            _client = client;
+        }
+
         [Command("avatar"), Summary("Displays the MentionedUser's full profile picture, or the command initiator's full pfp if no user is mentioned.")]
         public async Task Avatar(SocketUser MentionedUser = null)
         {
+            string url;
             if (MentionedUser != null)
             {
-                await ReplyAsync(MentionedUser.GetAvatarUrl(ImageFormat.Auto, 2048));
+                url = MentionedUser.GetAvatarUrl(ImageFormat.Auto, 2048);
             }
             else
             {
-                await ReplyAsync(Context.User.GetAvatarUrl(ImageFormat.Auto, 2048));
+                url = Context.User.GetAvatarUrl(ImageFormat.Auto, 2048);
             }
+            var imageStream = await _client.GetStreamAsync(url);
+            await Context.Channel.SendFileAsync(imageStream, new Uri(url).Segments[^1]);
         }
     }
 }
