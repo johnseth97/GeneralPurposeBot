ce11ba4c1655e3425069849fb0f169ebe082a8b4
Deso <johnseth97@gmail.com>
2020-04-29 02:45:31 -0400

    Fixed program.cs formatting and  made todo for http client

---
+++ Services/HttpClient.cs
@@ -0,0 +1,32 @@
+using System.Net.Http;
+using System;
+
+namespace GeneralPurposeBot.Services
+{
+    public class HttpClient
+    {
+    /* TODO- MAKE HTTP CLIENT
+        static readonly HttpClient _HttpClient = new HttpClient();
+
+        static async Task HttpClient(string[] args)
+        {
+           // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                HttpResponseMessage response = await _.GetAsync("http://www.contoso.com/");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }*/
+    }
+
+}
