1c3204bebcc32695e02babb6fb1bf4028c580915
Deso <johnseth97@gmail.com>
2020-04-29 02:46:49 -0400

    Added command to reply to users with the bot's github page

---
+++ Modules/GithubSource.cs
@@ -0,0 +1,16 @@
+using Discord.Commands;
+using System.Threading.Tasks;
+
+
+namespace GeneralPurposeBot.Modules
+{
+    public class GithubSource : ModuleBase
+    {
+        [RequireNsfw]
+        [Command("source"), Summary("Sends bot's github repo")]
+        public async Task GetSource()
+        {
+            await ReplyAsync("My Source Code is here! https://github.com/EthanJohnson97/GeneralPurposeBot/");
+        }
+    }
+}
