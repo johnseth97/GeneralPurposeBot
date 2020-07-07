4c91f49916a6e5759c5d5515d562f9e6fc0405ae
Mitchell Monahan <me@mitchellmonahan.com>
2020-07-06 20:36:26 -0400

    Temporary VCs can now be locked/unlocked

---
+++ Preconditions/RequireGuildAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeBot.Preconditions
+{
+    public class RequireGuildAttribute : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext ctx, CommandInfo command, IServiceProvider services)
+        {
+            if (ctx.Guild == null)
+                return Task.FromResult(PreconditionResult.FromError("You must be in a guild to run this command"));
+            return Task.FromResult(PreconditionResult.FromSuccess());
+        }
+    }
+}
