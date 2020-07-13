cef747a4d53c1496ac79611c2238f0ca56e25d56
Mitchell Monahan <me@mitchellmonahan.com>
2020-07-13 18:55:22 -0400

    Add disableable modules

--- Services/ServerPropertiesService.cs
+++ Services/ServerPropertiesService.cs
@@ -1,8 +1,10 @@
-﻿using GeneralPurposeBot.Models;
+﻿using Discord.Commands;
+using GeneralPurposeBot.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GeneralPurposeBot.Services
 {
@@ -35,6 +37,61 @@ namespace GeneralPurposeBot.Services
             entity = DbContext.ServerProperties.Update(entity).Entity;
             DbContext.SaveChanges();
             return entity;
+        }
+
+        public bool IsModuleEnabled(ModuleInfo module, ulong id)
+        {
+            var matches = DbContext.ServerModules
+                .AsQueryable()
+                .Where(sm => sm.ServerId == id && sm.Name == module.GetFullName());
+            if (matches.Any())
+                return !matches.First().Disabled;
+            return true;
+        }
+
+        public async Task EnableModule(ModuleInfo module, ulong id)
+        {
+            var matches = DbContext.ServerModules
+                .AsQueryable()
+                .Where(sm => sm.ServerId == id && sm.Name == module.GetFullName());
+            if (matches.Any())
+            {
+                var match = matches.First();
+                match.Disabled = false;
+            }
+            else
+            {
+                var match = new ServerModule()
+                {
+                    ServerId = id,
+                    Name = module.GetFullName(),
+                    Disabled = false
+                };
+                DbContext.ServerModules.Add(match);
+            }
+            await DbContext.SaveChangesAsync().ConfigureAwait(false);
+        }
+        public async Task DisableModule(ModuleInfo module, ulong id)
+        {
+            var matches = DbContext.ServerModules
+                .AsQueryable()
+                .Where(sm => sm.ServerId == id && sm.Name == module.GetFullName());
+            if (matches.Any())
+            {
+                var match = matches.First();
+                match.Disabled = true;
+            }
+            else
+            {
+                var match = new ServerModule()
+                {
+                    ServerId = id,
+                    Name = module.GetFullName(),
+                    Disabled = true
+                };
+                DbContext.ServerModules.Add(match);
+            }
+            await DbContext.SaveChangesAsync().ConfigureAwait(false);
         }
     }
 }
