775a4983c2902294386f08a9225b1e3b519e0a9e
Mitchell Monahan <me@mitchellmonahan.com>
2020-07-18 01:14:04 -0400

    Web UI for help stuff done

--- Web/Models/Help/Command.cs
+++ Web/Models/Help/Command.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 
 namespace GeneralPurposeBot.Web.Models.Help
@@ -13,11 +14,22 @@ namespace GeneralPurposeBot.Web.Models.Help
             Name = command.Name;
             Description = command.Summary;
             Aliases = command.Aliases;
+            Usage = "";
+            foreach (var param in command.Parameters)
+            {
+                Usage += param.IsOptional ? "[" : "<";
+                Usage += param.Type.Name;
+                Usage += " ";
+                Usage += param.Name;
+                Usage += param.IsOptional ? (" = " + param.DefaultValue != null ? param.DefaultValue : "null") : "";
+                Usage += param.IsOptional ? "] " : "> ";
+            }
+            Usage = Usage.Trim();
         }
 
         public string Name { get; set; }
         public string Description { get; set; }
         public IEnumerable<string> Aliases { get; set; }
-        public List<string> Usages { get; set; }
+        public string Usage { get; set; }
     }
 }
