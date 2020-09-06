4890217831c9ceb38a2ef1e4a0b3d6682d67043b
Deso <johnseth97@gmail.com>
2020-09-05 21:56:16 -0400

    Auto-generated database migrations

---
+++ Migrations/20200906004915_AssignableRoles.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GeneralPurposeBot.Migrations
+{
+    public partial class AssignableRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AssignableRoles",
+                columns: table => new
+                {
+                    RoleId = table.Column<ulong>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ServerId = table.Column<ulong>(nullable: false),
+                    RoleName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssignableRoles", x => x.RoleId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssignableRoles");
+        }
+    }
+}
