05f50c2c537944eb3d7e38c56c749b76a555ef55
Deso <johnseth97@gmail.com>
2020-09-16 02:54:44 -0400

    changed items to GameItems

--- Services/GameItemService.cs
+++ Services/GameItemService.cs
@@ -1,5 +1,5 @@
 ﻿using GeneralPurposeBot.Models;
-using GeneralPurposeBot.Services.Items;
+using GeneralPurposeBot.Services.GameItems;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Services/GameItems/DustItem.cs
+++ Services/GameItems/DustItem.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GeneralPurposeBot.Services.Items
+namespace GeneralPurposeBot.Services.GameItems
 {
     public class DustItem : ItemBase
     {
